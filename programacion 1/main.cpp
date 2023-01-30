69dafd0c48f10b33d184145ad3a304add393bc24
Luis <luisplanellah@gmail.com>
2023-01-30 10:01:16 +0100

    Initial Commit

---
+++ programacion 1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  programacion 1
+//
+//  Created by Sergio Planella Hernández on 30/1/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
